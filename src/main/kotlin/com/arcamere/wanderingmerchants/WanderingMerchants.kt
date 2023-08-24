932ec3a985f6241dac4af039d90bdfafc57bf04d
Connor Graham <thecg1997@gmail.com>
2023-08-24 14:14:49 +0100

    Initial commit

---
+++ src/main/kotlin/com/arcamere/wanderingmerchants/WanderingMerchants.kt
@@ -0,0 +1,13 @@
+package com.arcamere.wanderingmerchants
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WanderingMerchants: JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
