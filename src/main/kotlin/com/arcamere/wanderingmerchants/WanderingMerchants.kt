bcc23cba97ae6e7fef98adf33d5231eadd26feca
Connor Graham <thecg1997@gmail.com>
2023-11-17 21:49:30 +0000

    Remove Vault integration

--- src/main/kotlin/com/arcamere/wanderingmerchants/WanderingMerchants.kt
+++ src/main/kotlin/com/arcamere/wanderingmerchants/WanderingMerchants.kt
@@ -5,24 +5,14 @@ import com.arcamere.wanderingmerchants.config.ConfigSerialiser
 import com.arcamere.wanderingmerchants.location.MerchantLocationMap
 import com.arcamere.wanderingmerchants.merchant.Merchant
 import dev.sergiferry.playernpc.api.NPCLib
-import dev.sergiferry.playernpc.integration.integrations.Vault
-import net.milkbowl.vault.economy.Economy
 import org.bukkit.plugin.java.JavaPlugin
 
 class WanderingMerchants: JavaPlugin() {
     var merchants = ArrayList<Merchant>()
     var locations = MerchantLocationMap()
-    lateinit var vault: Economy
     override fun onEnable() {
         // Plugin startup logic
         this.saveDefaultConfig()
-
-        // Link Vault integration
-        if (!setupEconomy()) {
-            logger.severe(String.format("[WanderingMerchants] - Disabled due to no Vault dependency found!"))
-            server.pluginManager.disablePlugin(this)
-            return;
-        }
 
         val config = ConfigSerialiser().deserialise(this)
         this.merchants = config.merchants
@@ -35,14 +25,5 @@ class WanderingMerchants: JavaPlugin() {
     override fun onDisable() {
         // Plugin shutdown logic
         NPCLib.getInstance().unregisterPlugin(this)
-    }
-
-    private fun setupEconomy(): Boolean {
-        if (server.pluginManager.getPlugin("Vault") == null) {
-            return false
-        }
-        val rsp = server.servicesManager.getRegistration(Economy::class.java) ?: return false
-        vault = rsp.provider
-        return true
     }
 }
