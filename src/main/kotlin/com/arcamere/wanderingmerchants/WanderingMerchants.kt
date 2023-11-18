1fa0a07c237a8d83ce8535d31168968a55333587
Connor Graham <thecg1997@gmail.com>
2023-11-18 18:08:59 +0000

    Shuffle merchants on plugin load

--- src/main/kotlin/com/arcamere/wanderingmerchants/WanderingMerchants.kt
+++ src/main/kotlin/com/arcamere/wanderingmerchants/WanderingMerchants.kt
@@ -3,6 +3,7 @@ package com.arcamere.wanderingmerchants
 import com.arcamere.wanderingmerchants.commands.WanderingMerchantsCommand
 import com.arcamere.wanderingmerchants.config.Config
 import com.arcamere.wanderingmerchants.config.ConfigSerialiser
+import com.arcamere.wanderingmerchants.location.MerchantLocation
 import com.arcamere.wanderingmerchants.location.MerchantLocationMap
 import com.arcamere.wanderingmerchants.merchant.Merchant
 import dev.sergiferry.playernpc.api.NPCLib
@@ -22,6 +23,8 @@ class WanderingMerchants: JavaPlugin() {
 
         NPCLib.getInstance().registerPlugin(this)
         this.getCommand("wanderingmerchants")?.setExecutor(WanderingMerchantsCommand(this))
+
+        this.shuffleMerchants()
     }
 
     override fun onDisable() {
@@ -31,5 +34,31 @@ class WanderingMerchants: JavaPlugin() {
 
     override fun saveConfig() {
         ConfigSerialiser().serialise(Config(merchants, locations)).save(File(this.dataFolder, "config.yml"))
+    }
+
+    fun shuffleMerchants() {
+        val lastLocation = HashMap<Merchant, MerchantLocation>()
+        for (merchant in merchants) {
+            if (merchant.location == null) {
+                continue
+            }
+            lastLocation[merchant] = merchant.location!!
+            merchant.location = null
+        }
+
+        val availableLocations = locations.clone()
+
+        for (merchant in merchants.shuffled()) {
+            var location = availableLocations.random()
+            while (lastLocation.containsKey(merchant) && location == lastLocation[merchant] && availableLocations.size > 1) {
+                location = availableLocations.random()
+            }
+            merchant.location = location
+            availableLocations.remove(location)
+
+            if (availableLocations.size == 0) {
+                return
+            }
+        }
     }
 }
--- src/main/kotlin/com/arcamere/wanderingmerchants/commands/WanderingMerchantsCommand.kt
+++ src/main/kotlin/com/arcamere/wanderingmerchants/commands/WanderingMerchantsCommand.kt
@@ -20,29 +20,7 @@ class WanderingMerchantsCommand(private val plugin: WanderingMerchants): Command
 
         return when (args[0]) {
             "shuffle" -> {
-                val lastLocation = HashMap<Merchant, MerchantLocation>()
-                for (merchant in plugin.merchants) {
-                    if (merchant.location == null) {
-                        continue
-                    }
-                    lastLocation[merchant] = merchant.location!!
-                    merchant.location = null
-                }
-
-                val availableLocations = plugin.locations.clone()
-
-                for (merchant in plugin.merchants.shuffled()) {
-                    var location = availableLocations.random()
-                    while (lastLocation.containsKey(merchant) && location == lastLocation[merchant] && availableLocations.size > 1) {
-                        location = availableLocations.random()
-                    }
-                    merchant.location = location
-                    availableLocations.remove(location)
-
-                    if (availableLocations.size == 0) {
-                        return true
-                    }
-                }
+                plugin.shuffleMerchants()
                 sender.sendMessage("Shuffled merchants.")
                 true
             }
